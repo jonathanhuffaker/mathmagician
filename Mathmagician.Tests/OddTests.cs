7d81f90708d46a6b15842d9831e7b6be36057f29
jonathanhuffaker <jonathanhuffaker@gmail.com>
2016-02-29 17:24:19 -0800

    Updated project/solution files with odd classes

---
+++ Mathmagician.Tests/OddTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class OddTests
+    {
+
+
+        [TestMethod]
+        public void OddEnsureICanCreateInstance()
+        {
+            Odd my_odds = new Odd();
+            Assert.IsNotNull(my_odds);
+        }
+
+        [TestMethod]
+        public void OddEnsureICanGetFirst()
+        {
+            // Arrange
+            Odd my_odds = new Odd();
+
+            // Act
+            int actual = my_odds.GetFirst();
+            int expected = 2;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OddEnsureICanGetNext()
+        {
+            // Arrange
+            Odd my_odds = new Odd();
+
+            // Act
+            int actual = my_odds.GetNext(6);
+            int expected = 8;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void OddIntegerEnsureICanCreateASequenceOfTen()
+        {
+            // Arrange
+            Odd my_odds = new Odd();
+
+            // Act
+            int[] actual = my_odds.GetSequence(10);
+            int[] expected = new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
